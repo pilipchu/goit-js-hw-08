5f0bd7414a1286ed7b524e3d46565b679729dd73
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-02-23 18:09:55 +0200

    фвв

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const allElements = document.querySelectorAll('.item')
-console.log(`Number of categories: ${allElements.length}`)
-//  allElements.forEach((element) => console.log(`Category: ${element.firstElementChild.textContent} \n Elements: ${element.lastElementChild.children.length}`))
-allElements.forEach(function el(element) {
-    console.log(`Category: ${element.firstElementChild.textContent}`)
-    console.log(`Elements: ${element.lastElementChild.children.length}`)
-})
--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const textInput = document.querySelector('#name-input')
-const textSpan = document.querySelector('#name-output')
-textInput.addEventListener('input', (event) => {
-    if (textInput.value.trim() !== '') {
-        textSpan.textContent = event.target.value
-    } else {
-        textSpan.textContent = 'Anonymous'
-    }
-})
